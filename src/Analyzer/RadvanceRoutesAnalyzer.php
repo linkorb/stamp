8d47a468700785c0982328f2d6ebfbbc03b79782
Stephan Meijer <me@stephanmeijer.com>
2018-08-16 13:18:45 +0200

    Improve RadvanceRoutesAnalyzer

--- src/Analyzer/RadvanceRoutesAnalyzer.php
+++ src/Analyzer/RadvanceRoutesAnalyzer.php
@@ -3,20 +3,39 @@
 namespace Stamp\Analyzer;
 
 use Stamp\Model\Project;
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing\Route;
 
 class RadvanceRoutesAnalyzer extends Analyzer
 {
     public function analyze(Project $project): ?array
     {
+        // Things can fail in the bootstrap-file itself.
+        // Therefore handling Radvance with care.
         try {
-            $app = $this->getApp($project);
-            return ['route_collection' => $app['routes']];
+            if ($routes = $this->getAppRoutes($project)) {
+                return ['route_collection' => $this->convertRoutes($routes)];
+            } else {
+                return null;
+            }
         } catch(Exception $e) {
             return null;
         }
     }
 
-    private function getApp(Project $project)
+    private function convertRoutes(RouteCollection $routeCollection): array
+    {
+        return array_map(function(Route $route) {
+            return [
+                'path'     => $route->getPath(),
+                'methods'  => $route->getMethods(),
+                'defaults' => $route->getDefaults()
+            ];
+        }, $routeCollection->all());
+    }
+
+    private function getAppRoutes(Project $project): ?RouteCollection
     {
         $autoloadPath = $this->getFilepath($project, 'vendor/autoload.php');
 
@@ -24,18 +43,18 @@ class RadvanceRoutesAnalyzer extends Analyzer
             return null;
         }
 
-        require_once($autoload);
+        require_once($autoloadPath);
     
         $envPath = $this->getFilepath($project, '.env');
         if (file_exists($envPath)) {
-            $dotenv = new Symfony\Component\Dotenv\Dotenv();
+            $dotenv = new DotEnv();
             $dotenv->load($envPath);
         }
 
         $appPath = $this->getFilepath($project, 'app/bootstrap.php');
 
         if (file_exists($appPath)) {
-            return require($appPath);
+            return require($appPath)['routes'];
         } else {
             return null;
         }
